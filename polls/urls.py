24b2f7f7afe5abef7306c2b5917a246b5fa0812a
Josh Maccabee <josh@yipit.com>
2020-03-27 21:10:38 -0400

    Add views and new URLs for polls app.

--- polls/urls.py
+++ polls/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<uuid:question_id>/", views.detail, name="detail"),
+    path("<uuid:question_id>/results/", views.results, name="results"),
+    path("<uuid:question_id>/vote/", views.vote, name="vote"),
+]
