842d7f38d9fe6a79b5a72722768862edc6d26496
Josh Maccabee <josh@yipit.com>
2020-03-28 18:10:52 -0400

    Add choices to polls detail view. Fix typo in template.

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
 """
-Views should return either an HttpResponse or an Exception
+Views should return either an HttpResponse or an Exception.
+The render shortcut will return an HttpResponse object by default.
 """
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -27,10 +30,38 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question {}."
-    return HttpResponse(response.format(question_id))
-
-
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {}.".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST is a dictionary-like object letting you access data by key name
+        # in this case `choice` returns the ID of the selected choice,
+        # as a string, since request.POST always returns strings
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    # request.POST will raise a KeyError if a choice wasn't provided in the POST data
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a choice."},
+        )
+
+    # use the F() function to avoid race conditions where two users might
+    # vote before the model has saved the vote. F() references the current
+    # field value rather than the value when the object was queried.
+    #
+    # just be aware that F() assignments persist after save,
+    # so be careful saving an object twice after assigning with F()
+    selected_choice.votes = F("votes") + 1
+    selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    #
+    # HttpResponseRedirect takes 1 parameter (a URL to which the user will be redirected)
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
