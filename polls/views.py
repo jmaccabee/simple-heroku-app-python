7d0cc673a8f45818026e4159d8c43f159a336d3f
Josh Maccabee <josh@yipit.com>
2020-03-28 17:02:58 -0400

    Refactor polls index to use render instead of template loader and HttpResponse.

--- polls/views.py
+++ polls/views.py
@@ -2,16 +2,19 @@
 Views should return either an HttpResponse or an Exception
 """
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    # render takes the request object as its first argument,
+    # a template name as it's second argument,
+    # and a dictionary as its optional third argument (context)
+    # it returns an HttpResponse object of the template rendered with the context
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
