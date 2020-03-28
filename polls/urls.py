91b88d12f01b58f8cbd7851d513ba4a7f2c2a735
Josh Maccabee <josh@yipit.com>
2020-03-28 17:16:41 -0400

    Use URL template tags instead of hardcoded URL paths for more flexibility and DRYer code.

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # pass a name argument to enable using URL template tags
     path("", views.index, name="index"),
     path("<uuid:question_id>/", views.detail, name="detail"),
     path("<uuid:question_id>/results/", views.results, name="results"),
